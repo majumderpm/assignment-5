8c3134595823aaea3c35bc939844d42c36ad0739
majumderpm <pm19971118@gmail.com>
2022-08-23 13:04:38 +0530

    change text heading

--- js/app.js
+++ js/app.js
@@ -1,27 +1,28 @@
-const playerArray = []
+const playerArray = [];
 
-const count = 0;
+// const count = 0;
 const playerContainer = document.getElementById("playerContainer");
 
 const handleSelect = (item) => {
+    // console.log(item);
 
 
     if (playerArray.length <= 4) {
 
 
-        playerContainer.innerHTML = " "
+        playerContainer.innerHTML = " ";
 
         const playername = (item.parentNode.childNodes[1].innerText);
 
         playerArray.push(playername);
 
 
-        for (let index = 0; index < playerArray.length; index++) {
-            const element = playerArray[index];
+        for (let i = 0; i < playerArray.length; i++) {
+            const element = playerArray[i];
 
             const childLi = document.createElement("li");
             childLi.innerHTML = `
-            ${index + 1}. <span>${element}</span>
+            ${i + 1}. <span>${element}</span>
             `
             playerContainer.appendChild(childLi);
         }
